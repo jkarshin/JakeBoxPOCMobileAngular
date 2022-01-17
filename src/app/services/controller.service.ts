4ee81a990dd846337ba8a4ee67bf8b1e77595d2c
jkarshin <jtkarshin@gmail.com>
2022-01-16 23:59:49 -0800

    Got websocket connection logic to work

--- src/app/services/controller.service.ts
+++ src/app/services/controller.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@angular/core';
-import { ConnectionSettings } from '../model/connection-settings';
-import { WebSocketService } from './websocket.service';
+import { WebsocketService } from './websocket.service';
 
 @Injectable({ providedIn: 'root' })
 export class ControllerService {
-  constructor(private websocketService: WebSocketService) {}
+  constructor(private websocketService: WebsocketService) {}
 
   connect(name: string, url: string) {
-    let openObserver = {
-      next: () => {
+    this.websocketService.connect(url, {
+      onOpen: () => {
         // TODO, set up listener(s) and send connection request
         console.log('In custom onOpen');
       },
-    };
-    let closeObserver = {
-      next(closeEvent) {
+      onClose: (closeEvent: CloseEvent) => {
         // TODO, tear down listeners and route back to login screen (and show error dialog)
         console.log('In custom onClose');
       },
-    };
-    this.websocketService.connect(url, openObserver, closeObserver);
+      onError: (event: Event) => {
+        console.log('In custom onError');
+      },
+      onConnectionError: (error: any) => {
+        console.log('In custom onConnectionError');
+      },
+      onMessage: (messageEvent: any) => {
+        console.log('In custom onMessage');
+      },
+    });
   }
 }
--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,75 +1,44 @@
 import { Injectable } from '@angular/core';
-import { NextObserver, Observable, tap } from 'rxjs';
-import { WebSocketSubject } from 'rxjs/webSocket';
 
-/**
- * Quirky behavior to be aware of:
- *
- * The socket will automatically close once the last subscriber unsubscribes. Also,
- * a closed socket will automatically try to reconnect if it is subscribed to,
- * regardless of how it was closed. This includes situations where the intial connection
- * attempt failed.
- */
+export interface Handlers {
+  onOpen: () => any;
+  onClose: (closeEvent: CloseEvent) => any;
+  onError: (event: Event) => any;
+  onConnectionError: (error: any) => any;
+  onMessage: (messageEvent: any) => any;
+}
+
 @Injectable({ providedIn: 'root' })
-export class WebSocketService {
-  private socket$: WebSocketSubject<any>;
-  /*
-   * Calling class(es) can subscribe to this obeservable to listen for messages.
-   *
-   * It will be undefined before connect() is called.
-   */
-  public messages$: Observable<any>;
+export class WebsocketService {
+  private websocket: WebSocket;
 
-  /*
-   * Establishes a new connection if one is not already established (even if
-   * if the existing connection is for a different url!)
-   *
-   * Once a connection is closed, consumers will need to re-subscribe.
-   *
-   * TODO update this text for correctness
-   * The supplied openObserverArg will be invoked when the connection is established.
-   * The supplied closeObserverArg will be invoked when an open connection is closed. It will
-   * NOT be invoked if the initial connection attempt fails. Instaed, an onError notification
-   * will be sent through messages$.
-   */
-  public connect(
-    url: string,
-    openObserverArg: NextObserver<Event>,
-    closeObserverArg: NextObserver<CloseEvent>
-  ): void {
-    if (!this.socket$ || this.socket$.closed) {
+  constructor() {}
+
+  connect(url: string, handlers: Handlers) {
+    try {
       console.log(`Attempting to connect to: ${url}`);
-      this.socket$ = new WebSocketSubject({
-        url,
-        openObserver: {
-          next: (event) => {
-            console.log(`Successfully connected to ${url}`);
-            openObserverArg.next(event);
-          },
-        },
-        closeObserver: {
-          next(closeEvent) {
-            console.log(
-              `WS connection closed; code: ${closeEvent.code} reason: ${closeEvent.reason}`
-            );
-            closeObserverArg.next(closeEvent);
-          },
-        },
-      });
-      this.messages$ = this.socket$.pipe(
-        tap({
-          error: (error) =>
-            console.log(`Received error from websocket: ${error}`),
-        })
+      this.websocket = new WebSocket(url);
+      this.websocket.onopen = () => {
+        console.log('Websocket was opened successfully.');
+        handlers.onOpen();
+      };
+      this.websocket.onclose = (event) => {
+        console.log(`Websocket was closed: ${event}`);
+        handlers.onClose(event);
+      };
+      this.websocket.onerror = (event) => {
+        console.log(`Websocket received an error: ${event}`);
+        handlers.onError(event);
+      };
+      this.websocket.onmessage = (message) => {
+        console.log(`Websocket received message: ${message.data}`);
+        handlers.onMessage(message.data);
+      };
+    } catch (error) {
+      console.log(
+        `Encountered error while attempting to open Websocket: ${error}`
       );
+      handlers.onConnectionError(error);
     }
-  }
-
-  public sendMessage(msg: any) {
-    this.socket$.next(msg);
-  }
-
-  public close() {
-    this.socket$.error({ code: 1000, reason: 'Normal closure from app.' });
   }
 }
